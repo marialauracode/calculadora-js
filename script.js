c880c21c9acd770ffd872e5831c4a31806d5c5b0
Maria Laura Castro <cmarialaura599@gmail.com>
2025-08-07 13:28:53 -0300

    iniciando o script

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-// 1° PASSO: Selecionando os elementos do HTML
+// 1º PASSO: Selecionando os elementos do HTML
 
-const resultado = querySelector('.resultado') 
+const resultado = document.querySelector('.resultado')
+const botoes = document.querySelectorAll('.botoes button')
+
+// 2º PASSO: Criar as variáveis principais
+
+numAtual = ""
+primeiroNum = null
+operador = null
+limpar = false // quando TRUE, limpa o visor
+
+// 3º PASSO: Criar a função que atualiza o visor
+
+function atualizaResultado(limparVisor = false){
+
+    resultado.innerHTML = limparVisor ? 0 : numAtual.replace(".", ",")
+
+}
